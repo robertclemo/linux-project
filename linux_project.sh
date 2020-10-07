7a76d833de6b294056ee3a98f026e3a1ebdfa58c
megan kumar <dallasrt82@gmail.com>
2020-10-07 15:27:14 -0500

    Added Average Bill Pay Cal Feature

--- linux_project.sh
+++ linux_project.sh
@@ -24,7 +24,6 @@ sleep 3
 clear
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "==You are accessing the customer portal for Persona Century Corporation.=="
-<<<<<<< HEAD
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Thank you for entering your password, $name. Please wait while we connect you to the system."
 sleep 6
@@ -50,9 +49,45 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo "==You are accessing the customer portal for Persona Century Corporation.=="
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo " "
-=======
 sleep 6
 clear
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "==You are accessing the customer portal for Persona Century Corporation.=="
->>>>>>> 63842acc123062358cf0506faf2f12091d3d358b
+
+# this is megans code for average bill pay
+
+echo " "
+echo " "
+
+echo " Hello! Welcome to the Average Bill Pay Program calculator. \nTo start please find the last 5 month of your billing statment.\nLook where it says bill total and insert that number for each month. "
+sleep 5
+echo " "
+echo " "
+echo ".......AVERAGE BILL PAY CALCULATOR......."
+sleep 5
+echo " "
+echo " "
+echo " "
+echo "Please enter your first bill: "
+read a
+echo "Second bill: "
+read b
+echo "Third bill: "
+read c
+echo "Fourth bill: "
+read d
+echo "Fifth bill: "
+read e
+echo " Thank You your Average Bill Pay Program is being calculated"
+sleep 5
+clear
+
+sum=$(($a + $b + $c + $d + $e))
+avg=$(echo $sum / 5 | bc -l )
+
+echo "..THIS IS YOUR NEW AVERAGE BILL PAY.." 
+echo " "
+echo " "
+echo " The sum of these 5 bill is: " $sum
+echo " The average of these 5 bill is: " $avg
+sleep 5
