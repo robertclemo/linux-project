2bd5504113297ceb0a2790433b5f7e85318f9fb7
Robert Clemo <robertclemo@gmail.com>
2020-10-07 14:46:12 -0400

    Home Page Created

---
+++ linux_project.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# * Author: Robert Clemo *
+
+clear
+
+echo "Greetings, citizen."
+sleep 1
+echo " "
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "==You are accessing the customer portal for Persona Century Corporation.=="
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo " "
+sleep 4
+read -p "Enter your name. >> " name
+echo "Accessing your account, $name."
+sleep 6
+clear
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "==You are accessing the customer portal for Persona Century Corporation.=="
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+read -p "Please enter your password, $name. >> " password
+sleep 3
+clear
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "==You are accessing the customer portal for Persona Century Corporation.=="
+<<<<<<< HEAD
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Thank you for entering your password, $name. Please wait while we connect you to the system."
+sleep 6
+clear
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "==You are accessing the customer portal for Persona Century Corporation.=="
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Password accepted. What can we do for you today, $name?"
+echo " "
+echo "1.) Return to Home Page"
+echo "2.) Television"
+echo "3.) Phone"
+echo "4.) Website/Domains"
+echo "5.) Home Security"
+echo "6.) Internet"
+echo "7.) Streaming"
+echo "8.) Dial-Up"
+echo "9.) Support"
+echo "10.) Terminate this session."
+read -p "Please enter a number (1-10) from above. >> " choice
+clear
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "==You are accessing the customer portal for Persona Century Corporation.=="
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo " "
+=======
+sleep 6
+clear
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "==You are accessing the customer portal for Persona Century Corporation.=="
+>>>>>>> 63842acc123062358cf0506faf2f12091d3d358b
