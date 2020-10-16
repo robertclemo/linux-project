5427061db78ab3ce9f8ed4f9735f0e5a835cbf90
Bryan ROlle <bryanrolle41@gmail.com>
2020-10-16 17:21:16 -0400

    Contact Us Page

---
+++ contact_us.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+
+echo "-------------Contact Us------------"
+echo "Welcome to the Contact Us page!"
+sleep 1.75
+echo "Here you'll see the information provided to contact us via phone or email."
+sleep 1.75
+echo "Email: Comedygold808@gmail.com"
+sleep 1.75
+echo "Phone: 540-272-4498"
+sleep 1.75
+echo "Visit Us: 7211 TheGreatest Dr."
+
+#read -p "Would you like to go back to the Homepage? >>" user
+
+
