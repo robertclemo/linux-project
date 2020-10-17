320ffe59c61de638029b473aab58b5e39edf95ee
robertclemo <72224419+robertclemo@users.noreply.github.com>
2020-10-17 16:06:40 -0400

    Update homepage_experiment.sh
    added Bryan's contact us

--- homepage_experiment.sh
+++ homepage_experiment.sh
@@ -390,7 +390,17 @@ sleep 5
 
 	"C" | "c" )
 	echo " "
-	echo "Contact Us"
+	echo "-------------Contact Us------------"
+	echo "Welcome to the Contact Us page!"
+	sleep 1.75
+	echo "Here you'll see the information provided to contact us via phone or email."
+	sleep 1.75
+	echo "Email: Comedygold808@gmail.com"
+	sleep 1.75
+	echo "Phone: 540-272-4498"
+	sleep 1.75
+	echo "Visit Us: 7211 The Greatest Dr."
+
 	echo " "
 	echo "Press ENTER to continue."
 	read anykey
