391630a5e6e3cb30dfd9750cc1ef8534c2fe9deb
robertclemo <72224419+robertclemo@users.noreply.github.com>
2020-10-17 15:54:53 -0400

    Update homepage_experiment.sh
    added streaming

--- homepage_experiment.sh
+++ homepage_experiment.sh
@@ -58,6 +58,7 @@ echo "[Ca]ble"
 echo "[P]hone Service"
 echo "[Se]curity"
 echo "[Ca]mera"
+echo "[St]reaming"
 echo "[F]requently-Asked Questions"
 echo "[C]ontact Us"
 echo " "
@@ -325,6 +326,36 @@ sleep 5
 	echo "Thank you for your order of I360"
 	else  
 	echo "Enjoy your survialance experience if you bought one"
+	fi
+	echo " "
+	echo "Press ENTER to continue."
+	read anykey
+	;;
+	
+	"St" | "ST" | "sT" | )
+	echo " "
+	echo "-------Welcome to the Streaming Page!--------"
+	sleep 2
+	echo "We are partnered with Amazon and we offer Amazon prime!"
+	sleep 2 
+	echo "If you join this premium service you get: Free premium music and Amazon exclusive originals!"
+	sleep 2
+	echo "The premium music extenstion offers the all latest albums and various musical arists!"
+	sleep 2
+	echo "We offer the exclusive amazon orignals ranging from movies and tv shows!"
+	sleep 2
+	echo "We offer various live tv networks such as : COMEDYCENTRAL, ESPN, TNT, FOX, ETC."
+	sleep 2
+	echo "We offer various local stations such as: CBS, NBC, PBS, ABC, ETC." 
+
+
+	y=1
+	read -p "Would you like to join?  1 is yes, 2 is no. [1-2] >> " oof
+	if [ $oof -eq $y ] 
+	then 
+	echo "Great choice! We're glad you decided to join!"
+	else
+	echo "That's ok. Maybe next time!" 
 	fi
 	echo " "
 	echo "Press ENTER to continue."
