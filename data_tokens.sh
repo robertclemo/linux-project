658f17d5dbe51481ac48c77c1fdb572585048dec
Christhiggins <braxtondru@gmail.com>
2020-10-19 18:34:34 -0400

    data tokens page

---
+++ data_tokens.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+clear
+
+echo "------------------Get To Know Data Tokens!----------------------"
+sleep 2
+echo "---------------Get MORE Data, After Your Plan Is Up-------------"
+sleep 2
+echo "----------Data Tokens Restore Your Service To Full Speed!-------"
+sleep 2
+echo "Exceed your Speed? Get reduced Speed Til Next Cycle Then Purchase These"
+sleep 2
+echo "------Prices Start at $ 9 for 3GB and go up to $ 75 for 25GB------"
+sleep 2
+echo "Data Tokens Do NOT Expire And Remain Available Until They've been Used"
+
+
+y=1
+read -p "Are You Interested In More Data? 1 is yes, 2 is no. [1-2] >>" more
+if [ $more -eq $y ]
+then
+echo "Thanks For Your Purchase! Enjoy More High Speed Data!"
+else
+echo "There Available anytime 24/7, When YOU Need It!"
+fi
