1574ba0fa44333a8c9f28387322eb43f48883ade
robertclemo <72224419+robertclemo@users.noreply.github.com>
2020-10-19 18:44:38 -0400

    Update homepage_experiment.sh
    added data tokens

--- homepage_experiment.sh
+++ homepage_experiment.sh
@@ -63,6 +63,7 @@ echo "[Se]curity"
 echo "[Cam]era"
 echo "[St]reaming"
 echo "[Sa]tellite"
+echo "[T]okens"
 echo "[F]requently-Asked Questions"
 echo "[C]ontact Us"
 echo "[E]xit"
@@ -440,6 +441,34 @@ case "$service" in
 	echo "Excellent Decision! We Look Forward To Protecting You!"
 	else
 	echo "Not Now, That's Ok. Keep Us In Mind!"
+	fi
+	echo " "
+	echo "Press ENTER to continue."
+	read anykey
+	;;
+
+	"T" | "t"
+	echo " "
+	echo "------------------Get To Know Data Tokens!----------------------"
+	sleep 2
+	echo "---------------Get MORE Data, After Your Plan Is Up-------------"
+	sleep 2
+	echo "----------Data Tokens Restore Your Service To Full Speed!-------"
+	sleep 2
+	echo "Exceed your Speed? Get reduced Speed Til Next Cycle Then Purchase These"
+	sleep 2
+	echo "------Prices Start at $ 9 for 3GB and go up to $ 75 for 25GB------"
+	sleep 2
+	echo "Data Tokens Do NOT Expire And Remain Available Until They've been Used"
+
+
+	y=1
+	read -p "Are You Interested In More Data? 1 is yes, 2 is no. [1-2] >>" more
+	if [ $more -eq $y ]
+	then
+	echo "Thanks For Your Purchase! Enjoy More High Speed Data!"
+	else
+	echo "There Available anytime 24/7, When YOU Need It!"
 	fi
 	echo " "
 	echo "Press ENTER to continue."
