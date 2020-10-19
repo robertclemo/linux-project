3ba3d0dd6a12c74660b35172648a78e5d51308de
Christhiggins <braxtondru@gmail.com>
2020-10-19 17:37:21 -0400

    Satelitte page

---
+++ hompage_experiment.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+clear
+
+echo "--------------Welcome To The Satellite Alarm System Page------------"
+sleep 2
+echo "--------Top Industry Experts With Installation Of Alarm Systems-----"
+sleep 2
+echo "--Bringing Cutting Edge Technologies To Your Business Or Home Today-"
+sleep 2
+echo "-Monitor Alarm Conditions-Motion Detectors-Glass Break Sensors------"
+sleep 2
+echo "-Trigger Alarm Devices Based On Specific Events And Conditions And  "
+echo "-- InDoor Contacts from a single interface--"
+sleep 2
+echo "--24 Hour Service-Client Support And After-Hours Emergency Service--"
+sleep 2
+echo "Your Property Remains Secure If Phone Is Down, Power Out Or No Interne-" 
+sleep 2
+echo "Arm And Disarm From AnyWhere, Check System, Get Notified Of Activity"
+sleep 2 
+echo "--- Request A Consultation- Be Aware And In Control From Anywhere--"
+
+y=1
+read -p "Would You Like Our Trusted Alarm Sevice?  1 is yes, 2 is no. [1-2]  >>" cool
+if [ $cool -eq $y ]
+then
+echo "Excellent Decision! We Look Forward To Protecting You!"
+else
+echo "Not Now, That's Ok. Keep Us In Mind!"
+fi
+
