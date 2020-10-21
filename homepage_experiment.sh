1c93f6a6c6534e1fa8c8296db744650656797761
robertclemo <72224419+robertclemo@users.noreply.github.com>
2020-10-21 16:21:49 -0400

    Update homepage_experiment.sh
    added Marshall's code for location

--- homepage_experiment.sh
+++ homepage_experiment.sh
@@ -126,10 +126,22 @@ case "$service" in
 
 	"L" | "l" )
 	echo " "
-	echo "We are located at __________________"
-	echo " "
-        echo "Press ENTER to continue."
-        read anykey
+	echo "55555 Fiveity-Fifth Avenue, Connecticut"
+	read -p "Do you need the ZIP code to mail us something? Yes = 1, No = 2 >> " anything
+	if [ $anything -eq 1 ]
+	then
+	echo "no. mailbox is broken."
+
+	elif [ $anything -eq 2 ]
+	then
+	echo "good. :)"
+
+	else
+	echo "Invalid answer. Come on, people. It's not that hard to put 1 or 2!"
+	fi
+	echo ""
+	echo "Press ENTER to continue."
+	read anykey
 	;;
 
 	"B" | "b" )
